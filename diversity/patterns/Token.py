bb1c86f0778fa022054a9faab0cd0d4c9c340a45
Chantal Shaib <chantal@chantals-mbp.local>
2023-10-04 15:55:47 -0400

    Fix docstrings, names

--- diversity/patterns/Token.py
+++ diversity/patterns/Token.py
@@ -9,15 +9,15 @@ def token_patterns(
         n: int,
         top_n: int = 10
 ) -> List[Tuple[str, int]]:
-    """ Pulls out ngrams patterns in the data. 
+    """ Finds ngrams patterns in the data.
 
     Args:
-        data (list): data to run frequency.
-        n (int): n-gram length.
-        top_n (int, optional): top patterns to display. Defaults to 10.
+        data (List[str]): Data to run frequency counts on.
+        n (int): N-gram length.
+        top_n (int, optional): Top patterns to display. Defaults to 10.
 
     Returns:
-        list: sorted list of top n-gram patterns
+        List[Tuple[str, int]]: Sorted list of top n-gram patterns.
     """
 
     # treat data as one string 
@@ -29,5 +29,5 @@ def token_patterns(
     sorted_frequency = sorted(frequency.items(), key=lambda kv: kv[1], reverse=True)[:top_n]
 
     sorted_frequency = [(' '.join(x[0]), x[1]) for x in sorted_frequency]
-    
+
     return sorted_frequency

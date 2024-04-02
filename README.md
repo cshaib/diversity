2e4233f5d08fab29b57d211c51de0b66c0ffdec9
Chantal Shaib <chantal@chantals-mbp.local>
2024-04-02 14:30:00 -0400

    add function for ngram search, update docs

--- README.md
+++ README.md
@@ -42,16 +42,61 @@ print(cr, hs, nds)
 
 You can also visualize various ngram patterns using this library:
 ```python
-# get the token-level patterns
-patterns_token  =  token_patterns(outputs, ngram)
 
-# get the POS patterns
-joined_pos, tuples  =  get_pos(outputs)
-ngrams_pos  =  token_patterns(joined_pos, ngram)
+# get the part-of-speech patterns and matching text for a dataset
 
-# for the top n-gram patterns, cycle through and get the matching text
-text_matches  = {}
-for  pattern, _  in  ngrams_pos:
-	text_matches['pattern'] =  pos_patterns(tuples, pattern)
+text = [
+"I enjoy walking with my cute dog for the rest of the day, but this time it was hard for me to figure out what to do with it. When I finally looked at this for a few moments, I immediately thought.",
+"I enjoy walking with my cute dog. The only time I felt like walking was when I was working, so it was awesome for me. I didn't want to walk for days. I am really curious how she can walk with me", 
+"I enjoy walking with my cute dog (Chama-I-I-I-I-I), and I really enjoy running. I play in a little game I play with my brother in which I take pictures of our houses."
+]
+n = 5 
+top_n = 100
+
+patterns = extract_patterns(text, n, top_n)
+patterns
 ```
- 
+
+```sh
+{'PRP VBP VBG IN PRP$': {'I enjoy walking with my'},
+ 'VBP VBG IN PRP$ NN': {'enjoy walking with my cute'},
+ 'VBG IN PRP$ NN NN': {'walking with my cute dog', 'walking with my cute dog.'},
+ 'IN DT JJ NN PRP': {'for a few moments, I', 'in a little game I'},
+ 'IN PRP$ NN NN IN': {'with my cute dog for'},
+ 'PRP$ NN NN IN DT': {'my cute dog for the'},
+ 'NN NN IN DT NN': {'cute dog for the rest'},
+ 'NN IN DT NN IN': {'dog for the rest of'},
+ 'IN DT NN IN DT': {'for the rest of the'},
+ 'DT NN IN DT NN': {'the rest of the day,'},
+ 'NN IN DT NN CC': {'rest of the day, but'},
+ 'IN DT NN CC DT': {'of the day, but this'},
+ 'DT NN CC DT NN': {'the day, but this time'},
+ 'NN CC DT NN PRP': {'day, but this time it'},
+ 'CC DT NN PRP VBD': {'but this time it was'},
+ 'DT NN PRP VBD JJ': {'this time it was hard'},
+ 'NN PRP VBD JJ IN': {'time it was hard for'},
+ 'PRP VBD JJ IN PRP': {'it was hard for me'},
+ ...
+ }
+```
+The pattern matches above are based on the frequency seen across the *entire* datasets, i.e., a part-of-speech pattern is only a pattern if it appears in more than 1 text in the original input. We consider the top 100 part-of-speech patterns (sorted by frequency).
+
+Once you have the patterns dictionary, if you want to identify all the patterns contained in a single text from the input, you can do the following:
+
+```python
+idx = 2
+match_patterns(text[idx], patterns)
+```
+```sh
+[('PRP VBP VBG IN PRP$', 'I enjoy walking with my'),
+ ('VBP VBG IN PRP$ NN', 'enjoy walking with my cute'),
+ ('VBG IN PRP$ NN NN', 'walking with my cute dog'),
+ ('IN DT JJ NN PRP', 'in a little game I'),
+ ('VBP VBG IN PRP$ JJ', 'enjoy walking with my cute'),
+ ('VBG IN PRP$ JJ JJ', 'walking with my cute dog'),
+ ('IN PRP$ JJ JJ JJ', 'with my cute dog (Chama-I-I-I-I-I),'),
+ ('PRP$ JJ JJ JJ CC', 'my cute dog (Chama-I-I-I-I-I), and'),
+ ('JJ JJ JJ CC PRP', 'cute dog (Chama-I-I-I-I-I), and I'),
+ ...
+]
+```
---
+++ diversity/functions.py
@@ -0,0 +1,93 @@
+""" Functions for extracting patterns and matching text to patterns. """
+
+import numpy as np
+import itertools
+from typing import List, Optional
+from tqdm import tqdm
+from pattern import token_patterns, get_pos, pos_patterns
+from nltk.tokenize import sent_tokenize
+
+def extract_patterns(text: List[str], 
+                     n: int = 5,
+                     top_n: int = 100
+) -> dict:
+    """ Extracts text and part-of-speech patterns from text input. 
+        Used to return a dictionary of patterns and the corresponding text match. 
+    Args:
+        text (List[str]): List of strings to extract patterns from.
+        n (int, optional): N-gram size. Defaults to 5.
+        top_n (int, optional): Number of top patterns to extract. Defaults to 100.
+    Returns:
+        dict: Dictionary of patterns and their corresponding text.
+
+    Example Usage:
+    >>> text = ["The quick brown fox jumps over the lazy dog.", 
+                "The slow red fox walks on the hyper dog."]
+    >>> extract_patterns(text, 4)
+
+    {'DT JJ NN NN': {'The quick brown fox'},
+    'JJ NN NN VBZ': {'quick brown fox jumps'},
+    'NN NN VBZ IN': {'brown fox jumps over'},
+    'NN VBZ IN DT': {'fox jumps over the'},
+    'VBZ IN DT JJ': {'jumps over the lazy'},
+    'IN DT JJ NN': {'over the lazy dog.'},
+    'DT JJ JJ NN': {'The slow red fox'},
+    'JJ JJ NN NNS': {'slow red fox walks'},
+    'JJ NN NNS IN': {'red fox walks on'},
+    'NN NNS IN DT': {'fox walks on the'},
+    'NNS IN DT NN': {'walks on the hyper'},
+    'IN DT NN NN': {'on the hyper dog.'}}
+    """
+
+    # sentence tokenize then search for patterns in the entire list 
+    outputs = sent_tokenize(' '.join(text))
+
+    # get the token (word)-level patterns
+    patterns_token  =  token_patterns(outputs, n)
+
+    # get the part-of-speech patterns (only include top_n patterns)
+    joined_pos, tuples  =  get_pos(outputs)
+    ngrams_pos  =  token_patterns(joined_pos, n, top_n)
+
+    # for the top n-gram patterns, cycle through and get the matching text
+    text_matches  = {}
+
+    for  pattern, _  in  ngrams_pos:
+        text_matches[pattern] =  pos_patterns(tuples, pattern)
+    
+    return text_matches
+
+
+def match_patterns(text: str, 
+                   patterns: dict
+) -> List[tuple]:
+    """ Matches text to part-of-speech patterns extracted from the `extract_patterns` function.
+        Used to identify which patterns appears in a single input text. 
+    Args:
+        text (str): Text to match patterns to.
+        patterns (dict): Dictionary of patterns and their corresponding text.
+    Returns:
+        List[tuple]: List of tuples with the pattern and the text that matched.
+
+    Example Usage: 
+    >>> text = ["The quick brown fox jumps over the lazy dog.", 
+        "The slow red fox walks on the hyper dog.",
+        "The cranky blue cat scratches the calm fish." ]
+    >>> patterns = extract_patterns(text, 4)
+    >>> match_patterns(text[2], patterns)
+
+    [('DT NN JJ NN', 'The cranky blue cat'),
+    ('NN JJ NN VBZ', 'cranky blue cat scratches'),
+    ('JJ NN VBZ DT', 'blue cat scratches the'),
+    ('NN VBZ DT NN', 'cat scratches the calm'),
+    ('VBZ DT NN NN', 'scratches the calm fish.')]
+    """
+
+    matches  =  []
+
+    for pattern, text_match in patterns.items():
+        for substr in text_match:
+            if substr in text:
+                matches.append((pattern, substr))
+
+    return matches

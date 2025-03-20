caeb6fd0f07cd610fa2f927d649f5af07c5fe5a5
venkatasg <gvenkata1994@gmail.com>
2025-03-20 15:58:49 -0400

    Updated self-rep
    - used collections Counter to speed up counting.
    - counts unique ngrams per doc
    - fixed bug(?) where 1 is added to the sum for every n-gram in a doc, but it should be add-one to the overall sum inside the log.
    - added verbosity parameter

--- diversity/self_repetition.py
+++ diversity/self_repetition.py
@@ -1,35 +1,41 @@
 import numpy as np
-import nltk
+from nltk.util import ngrams
 
 from tqdm import tqdm
 from typing import List
+from collections import Counter
 
 def self_repetition_score(
         dataset: List[str],
-        n: int = 4
+        n: int = 4,
+        verbose: bool = True
 ) -> float:
-    """Calculates a self-repetition score for a dataset based on the 
+    """
+    Calculates a self-repetition score for a dataset based on the 
     repetition of ngrams within the corpus.
 
     Args:
         dataset (List[str]): A list of documents (strings) to analyze.
         n (int): Size of the ngrams to check for repetition. Defaults to 4.
+        verbose (bool): enable/disable show progress bar
 
     Returns:
         float: The self-repetition score, averaged over the dataset.
     """
     total_sum = 0
+    
+    # Get all unique ngrams per doc
+    ngram_docs = [list(set([' '.join(ngram) for ngram in ngrams(doc.split(), 4)])) for doc in dataset]
+    
+    # Count occurrences of unique ngrams across whole dataset
+    all_ngrams = sum(ngram_docs, [])
+    ngram_counts = Counter(all_ngrams)
 
-    for doc in tqdm(dataset, desc="Calculating self-repetition score"):
-        ngrams = [' '.join(ngram) for ngram in nltk.ngrams(doc.split(), n)]
-        sum_ni = 0
-
-        for ngram in ngrams:
-            # Count occurrences of ngram in other documents (excluding current doc)
-            n_i = sum(1 for d in dataset if ngram in d and d != doc)
-
-            sum_ni += (n_i + 1)
-
-        total_sum += np.log(sum_ni)
-
+    for ngram_doc in tqdm(ngram_docs, desc="Calculating self-repetition score", disable=(not verbose)):
+        # Find the total occurrence of an n-gram and subtract current doc's n-gram count
+        # to get the count of occurrences of an n-gram in other docs
+        sum_ni = sum([ngram_counts[ngram] for ngram in ngram_doc]) - len(ngram_doc)
+        
+        # add-one to avoid zero error
+        total_sum += np.log(sum_ni + 1)
     return total_sum / len(dataset)

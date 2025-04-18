3392a810f48800c0f9226efec83bc9b6423128e0
venkatasg <gvenkata1994@gmail.com>
2025-04-18 16:22:26 -0400

    Homogenization score optimizations
    - BERTScore is now calculated in a batched way
    - Overall code is re-written to be cleaner to read and understand.
    - tests for timing and checking correctness of homogenzation scores

---
+++ diversity/hom_tests.py
@@ -0,0 +1,25 @@
+from datasets import load_dataset
+from homogenization import homogenization_score
+from time import perf_counter as pc
+from numpy import round
+
+if __name__ == '__main__':
+    data = load_dataset("argilla/cnn-dailymail-summaries")["train"].to_pandas().highlights.sample(500, random_state=1).values.tolist()
+    
+    start = pc()
+    bs = homogenization_score(data, measure='bertscore', verbose=True, model="distilbert-base-uncased")
+    end = pc()
+    
+    print(f"Time taken to compute BERTScore over CNN summaries: {round(end-start,2)}\nBERTScore over 500 reference summaries: {bs}")
+    
+    start = pc()
+    rl = homogenization_score(data, measure='rougel', verbose=True)
+    end = pc()
+    
+    print(f"Time taken to compute Rouge over CNN summaries: {round(end-start,2)} secs\nRouge over 500 reference summaries: {rl}")
+    
+    start = pc()
+    rl = homogenization_score(data, measure='bleu', verbose=True)
+    end = pc()
+    
+    print(f"Time taken to compute BLEU over CNN summaries: {round(end-start,2)} secs\nBLEU over 500 reference summaries: {rl}")
--- diversity/homogenization.py
+++ diversity/homogenization.py
@@ -1,18 +1,15 @@
-import numpy as np
-import itertools
-from diversity import memoized
 from typing import List, Optional
 from tqdm import tqdm
 from rouge_score import rouge_scorer
 from evaluate import load
-from multiprocessing import Pool, cpu_count
 
 def homogenization_score(
         data: List[str],
         measure: str = 'rougel',
         use_stemmer: Optional[str] = False,
-        model: Optional[str] = 'distilbert-base-uncased',
-        verbose: Optional[bool] = True
+        model: Optional[str] = "distilbert-base-uncased",
+        verbose: Optional[bool] = True,
+        batch_size: Optional[int] = 64
 ) -> float:
     """ 
     Calculates the homogenization score for a set of documents (corpus-level). 
@@ -21,7 +18,7 @@ def homogenization_score(
          data (List[str]): Strings to score.
          measure (str, optional): Either 'rougel', 'bertscore', or 'bleu'. Defaults to 'rougel'.
          use_stemmer(str, optional): Whether to use stemming in the ROUGE-L calculation. Defaults to False.
-         model(str, optional): Model to use for BERTScore. Defaults to 'microsoft/deberta-xlarge-mnli'. 
+         model(str, optional): Model to use for BERTScore. Defaults to 'microsoft/deberta-base-mnli'. 
          verbose(bool, optional): Whether to display progress bar. Defaults to True.
      Returns:
          float: Homogenization score.
@@ -37,49 +34,33 @@ def homogenization_score(
         raise ValueError("Scoring measure must be one of `rougel`, `bleu`, or `bertscore`.")
 
     corpus_score = 0
-    doc_score = 0
     
     if verbose:
         print('==> Scoring all pairs')
-        
-    pool = Pool(cpu_count()-1) 
      
-    for ind1, curr_str in tqdm(enumerate(data), total=len(data), disable=(not verbose)):
-        all_args = [((curr_str, data[ind2]), scorer, measure, model) for ind2 in range(len(data)) if ind2!=ind1] 
-        doc_scores = pool.map(wrapped_score, all_args)
-        corpus_score += sum(doc_scores)/(len(data)-1)
+    for i, ref  in tqdm(enumerate(data), total=len(data), disable=(not verbose)):
+        # Get all the other utterances to compare against a specific utterance
+        preds = [x for j,x in enumerate(data) if j!=i]
+        refs = [ref for _ in range(len(preds))]
+        
+        # Get scores over whole batch and sum it up
+        if measure=='rougel':
+            doc_score = sum([scorer.score(pred, ref)['rougeL'].fmeasure for pred in preds])
+        elif measure=='bertscore':
+            doc_score = sum(scorer.compute(predictions=preds, 
+                                           references=refs, 
+                                           model_type=model, 
+                                           batch_size=batch_size)['f1'])
+        elif measure=='bleu':
+            # Need to double check that this is right
+            doc_score = scorer.compute(predictions=preds, 
+                                       references=[[r] for r in refs])['bleu']
+        # Then average
+        corpus_score += doc_score / (len(data) - 1)
     
     # case where all strings are the exact same in the list
     if corpus_score == 0: 
         corpus_score += len(data)
     
     # returns corpus level homogenization score 
-    return round(corpus_score / len(data), 3)
-
-def wrapped_score(args): 
-    """
-    we need to wrap the call to unpack the parameters 
-    we build before as a tuple for being able to use pool.map
-    """ 
-    return _calculate_score(*args)
-
-@memoized
-def _calculate_score(pair, scorer, measure, model):
-    """ Returns the score of two strings """
-    
-    # repeated string (excat str match) that is *not* at the same position 
-    if pair[0] == pair[1]: 
-        return 1
-    else:
-        if measure == 'rougel':
-            score = scorer.score(pair[0], 
-                                 pair[1])['rougeL'][-1]
-        elif measure == 'bertscore': 
-            score = scorer.compute(predictions=[pair[0]], 
-                                   references=[pair[1]], 
-                                   model_type=model)['f1'][0]
-        elif measure == 'bleu': 
-            score = scorer.compute(predictions=[pair[0]], 
-                                   references=[pair[1]])['bleu']
-        
-        return score 
+    return round(corpus_score/len(data), 3)

619b9095736537d78bc48e744a8076e40b10af24
Chantal S <chantal.shaib@gmail.com>
2025-08-18 10:59:36 -0400

    add placeholder for new functions, move tests

---
+++ diversity/template.py
@@ -0,0 +1,15 @@
+from typing import List 
+
+def template_rate(
+        data: List[str],
+        template: str,
+        
+) -> float:
+    pass 
+
+
+def templates_per_token(
+        data: List[str],
+        templates: List[str]
+) -> List[float]:
+    pass
